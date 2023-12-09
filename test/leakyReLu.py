933d35b710d7e77178eb09e18921afb096dc1bef
Mouna Krishna <mounakrishna@mindgrovetech.in>
2023-12-09 22:26:01 +0530

    Corrected the reference model for leakyRelu

--- test/leakyReLu.py
+++ test/leakyReLu.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import math
 
 bias = int(sys.argv[1])
 possible_inp = {}
@@ -13,31 +14,42 @@ def leakyRelu(inp):
     else:
         return 0.01 * inp
 
-def round_to_nearest(a, b, inp):
+def round_to_nearest(a, b, manA, manB, inp):
     mid = (a+b)/2
     if (inp >= a and inp < mid):
-        return a
+        return a, manA
     else:
-        return b
+        return b, manB
 
 def decimal_to_cfloat(inp):
     #print(inp)
+    inp = abs(inp)
     if (inp >= 0 and inp < denormal_inp[1]):
-        print(round_to_nearest(0, denormal_inp[1], inp))
+        return (round_to_nearest(0, denormal_inp[1], 0, 1, inp), 0)
     elif (inp >= denormal_inp[1] and inp < denormal_inp[2]):
-        print(round_to_nearest(denormal_inp[1], denormal_inp[2], inp))
+        return (round_to_nearest(denormal_inp[1], denormal_inp[2], 1, 2, inp), 0)
     elif (inp >= denormal_inp[2] and inp < denormal_inp[3]):
-        print(round_to_nearest(denormal_inp[2], denormal_inp[3], inp))
+        return (round_to_nearest(denormal_inp[2], denormal_inp[3], 2, 3, inp), 0)
     elif (inp >= denormal_inp[3] and inp < possible_inp[1][0]):
-        print(round_to_nearest(denormal_inp[2], possible_inp[1][0], inp))
+        tmp1, tmp2 = round_to_nearest(denormal_inp[3], possible_inp[1][0], 3, 0, inp)
+        if tmp2 == 3:
+            return (tmp1, 3), 0
+        else:
+            return (tmp1, 0), 1
     else:
         for exp in range(1, 32):
             if (inp >= possible_inp[exp][0] and inp < possible_inp[exp][1]):
-                print(round_to_nearest(possible_inp[exp][0], possible_inp[exp][1], inp))
+                return (round_to_nearest(possible_inp[exp][0], possible_inp[exp][1], 0, 1, inp), exp)
             elif (inp >= possible_inp[exp][1] and inp < possible_inp[exp][2]):
-                print(round_to_nearest(possible_inp[exp][1], possible_inp[exp][2], inp))
+                return (round_to_nearest(possible_inp[exp][1], possible_inp[exp][2], 1, 2, inp), exp)
             elif (inp >= possible_inp[exp][2] and inp < possible_inp[exp][3]):
-                print(round_to_nearest(possible_inp[exp][2], possible_inp[exp][3], inp))
+                return (round_to_nearest(possible_inp[exp][2], possible_inp[exp][3], 2, 3, inp), exp)
+            elif (inp >= possible_inp[exp][3] and inp < possible_inp[exp+1][0]):
+                tmp1, tmp2 = round_to_nearest(possible_inp[exp][3], possible_inp[exp+1][0], 3, 0, inp)
+                if tmp2 == 3:
+                    return (tmp1, 3), exp
+                else:
+                    return (tmp1, 0), exp+1
             
 
 def gen_inp_cfloat_numbers():
@@ -45,7 +57,7 @@ def gen_inp_cfloat_numbers():
         value = 2**(exp - bias)
         for m in range(4):
             if (m == 0):
-                possible_inp[exp].append(value * 1)
+                possible_inp[exp].append(value * 1.0)
             elif (m == 1):
                 possible_inp[exp].append(value * 1.25)
             elif (m == 2):
@@ -64,5 +76,30 @@ def gen_inp_cfloat_numbers():
 
 if __name__=="__main__":
     gen_inp_cfloat_numbers()
-    number = 2.1
-    decimal_to_cfloat(leakyRelu(number))
+    #for exp in range(1, 32):
+    #    print(possible_inp[exp])
+    print(possible_inp[2][0])
+    print(decimal_to_cfloat(possible_inp[2][0]))
+    print(decimal_to_cfloat(leakyRelu(-1*possible_inp[2][0])))
+    #number = -37.5
+    ref_file = open("leakyReLu_ref.txt", "w")
+
+    for exp in range(1, 32):
+        for m in range(4):
+            #print(exp, m)
+            #print(decimal_to_cfloat(possible_inp[exp][m]))
+            (inp_dec, inp_man), inp_exp = decimal_to_cfloat(possible_inp[exp][m])
+            (out_dec, out_man), out_exp = decimal_to_cfloat(leakyRelu(possible_inp[exp][m] * -1))
+            print("exp: ", exp, "m: ", m, "inp: ", possible_inp[exp][m])
+            print("inp_dec: ", inp_dec ," inp_man: ", inp_man," inp_exp: ", inp_exp)
+            print("out_dec: ", out_dec ," out_man: ", out_man," out_exp: ", out_exp)
+            ref_file.write(str(inp_dec) + ' ' + str(inp_man) + ' ' + str(inp_exp) + ' ' + str(out_dec) + ' ' + str(out_man) + ' ' + str(out_exp) + '\n')
+
+    ref_file.close()
+
+    #(inp_dec, inp_man), inp_exp = decimal_to_cfloat(abs(number))
+    #(out_dec, out_man), out_exp = decimal_to_cfloat(leakyRelu(number))
+
+    #print("inp_dec: ", inp_dec ," inp_man: ", inp_man," inp_exp: ", inp_exp)
+    #print("out_dec: ", out_dec ," out_man: ", out_man," out_exp: ", out_exp)
+    #print("OUT: ", decimal_to_cfloat(leakyRelu(number)))
